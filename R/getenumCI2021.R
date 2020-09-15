9445ee912c2e6e0ef3e36e169017c3cdfe579881
Gabriel Bassett <gabe@infosecanalytics.com>
2020-09-15 18:27:58 -0500

    fixes to flatten_verisr().

--- R/getenumCI2021.R
+++ R/getenumCI2021.R
@@ -9,25 +9,30 @@ flatten_verisr <- function(veris) {
   # first separate duplicate from  non-duplicate master_ids
   rows_per_master_id <- table(veris$plus.master_id) # this is slow
   
-  ret <- dplyr::bind_rows( # using dplyr here to try and 
-    veris[veris[["plus.master_id"]] %in% names(rows_per_master_id[rows_per_master_id == 1])], # rows that don't need to be changed
-    veris %>%
-      dplyr::summarize(
-        dplyr::across(dplyr::where(is.logical), any, na.rm=TRUE),
-        dplyr::across(dplyr::where(is.numeric), sum, na.rm=TRUE),
-        dplyr::across(dplyr::where(is.character), function(c) {
-          unique_c <- unique(c)
-          if (length(unique_c) > 1) {warning(paste0("First of ", paste(unique_c, collapse=","), " kept during flattening."))}
-          return(unique_c[1])
-        }),
-        dplyr::across(dplyr::where(is.factor), function(c) {
-          c <- as.character(c) # convert to character and treat as normal from there. - catch for patterns as factor.
-          unique_c <- unique(c)
-          if (length(unique_c) > 1) {warning(paste0("First of ", paste(unique_c, collapse=","), " kept during flattening."))}
-          return(unique_c[1])
-        })
-      )
+  ret <- veris[veris[["plus.master_id"]] %in% names(rows_per_master_id[rows_per_master_id > 1]), ]
+  ret <- dplyr::group_by(ret, plus.master_id)
+  ret <- dplyr::summarize(ret,
+                          dplyr::across(where(is.logical), any, na.rm=TRUE),
+                          dplyr::across(where(is.numeric), sum, na.rm=TRUE),
+                          dplyr::across(where(is.character), function(c) {
+                            unique_c <- unique(c)
+                            if (length(unique_c) > 1) {warning(paste0("First of ", paste(unique_c, collapse=","), " kept during flattening."))}
+                            return(unique_c[1])
+                          }),
+                          dplyr::across(where(is.factor), function(c) {
+                            c <- as.character(c) # convert to character and treat as normal from there. - catch for patterns as factor.
+                            unique_c <- unique(c)
+                            if (length(unique_c) > 1) {warning(paste0("First of ", paste(unique_c, collapse=","), " kept during flattening."))}
+                            return(unique_c[1])
+                          })
   )
+  
+  
+  ret <- dplyr::bind_rows( # using dplyr here to try and 
+    veris[veris[["plus.master_id"]] %in% names(rows_per_master_id[rows_per_master_id == 1]), ], # rows that don't need to be changed
+    ret
+  )
+
   
   return(ret)
 }
